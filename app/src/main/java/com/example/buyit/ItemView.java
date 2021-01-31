c43147703c76b21a16c6b8450167f94e61929d62
andy ung <andyung123456789@gmail.com>
2021-01-30 23:38:23 -0500

    Updated look

--- app/src/main/java/com/example/buyit/ItemView.java
+++ app/src/main/java/com/example/buyit/ItemView.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ItemView extends AppCompatActivity {
 
@@ -12,6 +14,12 @@ public class ItemView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_view);
+
+        TextView header = (TextView)findViewById(R.id.itemValue);
+
+        Bundle b = getIntent().getExtras();
+        header.setText("Item search: " + b.getString("key"));
+
     }
 
     public void backButton(View view) {
--- app/src/main/java/com/example/buyit/myList.java
+++ app/src/main/java/com/example/buyit/myList.java
@@ -11,4 +11,8 @@ public class myList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_list);
     }
+
+
+
+
 }
