c71524bcbe52ac58ac20b5fb3bb244147287b1de
andy ung <andyung123456789@gmail.com>
2021-01-30 21:24:39 -0500

    Improved Overall UI Design

--- app/src/main/java/com/example/buyit/MainActivity.java
+++ app/src/main/java/com/example/buyit/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.buyit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void searchItem(View view) {
+        Intent intent = new Intent (getApplicationContext(), ItemView.class);
+        startActivity(intent);
+    }
+
+    public void myList(View view) {
+        Intent intent = new Intent (getApplicationContext(), myList.class);
+        startActivity(intent);
+    }
+
+    public void availableView(View view) {
+        Intent intent = new Intent (getApplicationContext(), availableItems.class);
+        startActivity(intent);
+    }
+
 }
